e1d5b8bb69de9caebce4bde025feaa8f4d23dcb2
Xuzeipan <1435157300@qq.com>
2020-12-27 09:46:10 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/*
+* 入口JS
+* */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+/* eslint-disable no-new */
+new Vue({
+  el:'#app',
+  render:h => h(App),
+  router
+})
---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+/*
+* 路由器对象模块
+* */
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Msite from '../pages/Msite/Msite'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Search from '../pages/Search/Search'
+import Login from '../pages/Login/Login'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes:[
+    {
+      path:'/',
+      redirect:'/msite'
+    },
+    {
+      path: '/msite',
+      component:Msite,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/order',
+      component:Order,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/search',
+      component:Search,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/profile',
+      component:Profile,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/login',
+      component:Login
+    }
+  ]
+})
